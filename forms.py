8a3d60f285ab722026f1aa1ab095bf170f82e6a7
IDGS801-21002128 <21002128@alumnos.utleon.edu.mx>
2024-02-07 19:04:51 -0600

    07/02/2024 Examen Practico PMA

--- forms.py
+++ forms.py
@@ -1,8 +1,13 @@
-from wtforms import Form,FloatField
+from wtforms import Form,FloatField,SelectField,RadioField
 
 class Punto(Form):
     x = FloatField('Punto x')
     y = FloatField('Punto y')
     x2 = FloatField('Punto x2')
     y2 = FloatField('Punto y2')
-   
+
+class Resistencias(Form):
+    PrimerResistencia = SelectField("Primer Resistencia")
+    SegundaResistencia = SelectField("Segunda Resistencia")
+    TerceraResistencia = SelectField("Tercer Resistencia")
+    tolerancia = RadioField("Tolerancia")
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import *
 from math import *
-from forms import Punto
+from forms import Resistencias
 app=Flask(__name__)
 
 
@@ -21,6 +21,48 @@ def calDistancia():
         resultado=sqrt((x2-x)**2 + (y2-y)**2)
         
     return render_template("distancia.html",form=distanciaClase,x=x,y=y,resultado=resultado)
+
+@app.route("/resis",methods=["GET","POST"])
+def calResis():
+    resistencia = Resistencias(request.form)
+    R1=''
+    R2=''
+    R3=''
+    oro=''
+    plata=''
+    resultado=''
+    junto = ''
+    colores = {"Negro":0,"Cafe":1,"Rojo":2,"Naranja":3,"Amarillo":4,"Verde":5,"Azul":6,"Violeta":7,"Gris":8,"Blanco":9}
+    multiplicador = {"Negro":1,"Cafe":10,"Rojo":100,"Naranja":1000,"Amarillo":10000,"Verde":100000,"Azul":1000000,"Violeta":10000000,"Gris":100000000,"Blanco":1000000000}
+    fin =''
+    max=''
+    min=''
+    tol=''
+    if request.method == "POST":
+        R1 = resistencia.PrimerResistencia.data
+        R2 = resistencia.SegundaResistencia.data
+        R3 = resistencia.TerceraResistencia.data
+        tolerancia = resistencia.tolerancia.data
+
+        junto = str(colores[R1]) + str(colores[R2])
+        
+        resultado = int(junto) * multiplicador[R3]
+        print(resultado)
+        print()
+        if tolerancia == "oro":
+            tol=tolerancia
+            fin = resultado*0.05
+        elif tolerancia=="plata":
+            tol=tolerancia
+            fin = resultado*0.10
+        else:
+            fin =resultado
+        
+        max=resultado+fin
+        min=resultado-fin
+      
+
+    return render_template("resistencia.html",form=resistencia, tol=tol,colores=colores, R1=R1, R2=R2,R3=R3,oro=oro,plata=plata,multiplicador=multiplicador,max=max,min=min,resultado=resultado,fin=fin)
 
 
     
